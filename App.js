3ea9c20897b0c5156d77aeb3f7ceb7f329b1b0d2
Sivadharshan AG <ag.sivadharshan@gmail.com>
2025-10-17 12:01:29 +0530

    Update App Key variable name in App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./styles.css";
 import DropboxChooser from "react-dropbox-chooser";
 
-const APP_KEY = "Paste the App Key";
+const APP_KEY = "Paste the AppKey";
 
 export default function App() {
   const [urls, setUrls] = useState([]);
